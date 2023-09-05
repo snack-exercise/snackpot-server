3a87fece2a7c3fc1cf3880d134ae8ba4ea502131
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-06 02:16:28 +0900

    [SNK-67] feat: 멀티 모듈 프로젝트 구성

---
+++ snackpot-api/src/main/java/com/soma/SnackpotApiApplication.java
@@ -0,0 +1,12 @@
+package com.soma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackpotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnackpotApiApplication.class, args);
+    }
+}
