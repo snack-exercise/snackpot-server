c1f790584da5728c1006181808dbc68a1067cf80
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-14 01:13:26 +0900

    [SNK-48] feat : 회원 엔티티 builder에 모든 필드 추가

--- snackpot-domain/src/main/java/com/soma/member/entity/Member.java
+++ snackpot-domain/src/main/java/com/soma/member/entity/Member.java
@@ -29,10 +29,15 @@ public class Member extends BaseEntity {
     private RoleType roleType;
 
     @Builder
-    public Member(String email, String name, Integer dailyGoalTime) {
-        this.email = email;
+    public Member(String name, String email, Integer dailyGoalTime, String profileImg, String fcmToken, String password, RoleType roleType) {
         this.name = name;
+        this.email = email;
         this.dailyGoalTime = dailyGoalTime;
+        this.profileImg = profileImg;
+        this.fcmToken = fcmToken;
+        this.password = password;
+        this.roleType = roleType;
+        active();
     }
 
     public void updateFcmToken(String fcmToken){
