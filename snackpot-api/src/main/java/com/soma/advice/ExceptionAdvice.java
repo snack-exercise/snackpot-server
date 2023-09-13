bf9ca93f96dfa4068c985acf23a8354d89a777a1
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-14 01:02:51 +0900

    [SNK-48] feat : MemberNotFoundException Handler 추가

--- snackpot-api/src/main/java/com/soma/advice/ExceptionAdvice.java
+++ snackpot-api/src/main/java/com/soma/advice/ExceptionAdvice.java
@@ -1,10 +1,25 @@
 package com.soma.advice;
 
+import com.soma.exception.MemberNotFoundException;
+import com.soma.util.response.Response;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.soma.advice.ErrorCode.MEMBER_NOT_FOUND_EXCEPTION;
 
 @Getter
 @RequiredArgsConstructor
+@RestControllerAdvice
 public class ExceptionAdvice {
+    @ExceptionHandler(MemberNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response MemberNotFoundExceptionHandler(MemberNotFoundException e){
+//        Sentry.captureException(e);
+        return Response.failure(MEMBER_NOT_FOUND_EXCEPTION);
+    }
 
 }
