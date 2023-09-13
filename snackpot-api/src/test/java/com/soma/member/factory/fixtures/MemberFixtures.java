7413aea415c9e73d239079063304f4db108506f6
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-14 01:23:14 +0900

    [SNK-48] feat : 테스트용 Member 고정변수 클래스 추가

---
+++ snackpot-api/src/test/java/com/soma/member/factory/fixtures/MemberFixtures.java
@@ -0,0 +1,17 @@
+package com.soma.member.factory.fixtures;
+
+import com.soma.member.entity.RoleType;
+
+import static com.soma.member.entity.RoleType.*;
+
+public class MemberFixtures {
+    public static String 이름 = "김누구";
+    public static Integer 하루목표운동시간 = 10;
+    public static String 이메일 = "test@naver.com";
+    public static String 프로필사진 = "profile_image";
+    public static String 비밀번호 = "password";
+    public static RoleType 일반권한 = USER;
+    public static RoleType 게스트권한 = GUEST;
+    public static String FCM토큰 = "sample_fcm_token";
+
+}
