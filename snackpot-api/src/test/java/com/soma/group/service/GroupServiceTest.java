1d8da27287e444bb2d94d0a0572c13058f18e16d
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-14 23:17:27 +0900

    [SNK-73] feat : 그룹 가입 Service 단위 테스트

--- snackpot-api/src/test/java/com/soma/group/service/GroupServiceTest.java
+++ snackpot-api/src/test/java/com/soma/group/service/GroupServiceTest.java
@@ -1,9 +1,12 @@
 package com.soma.group.service;
 
+import com.soma.exception.group.AlreadyJoinedGroupException;
 import com.soma.group.dto.request.GroupCreateRequest;
-import com.soma.group.dto.response.GroupCreateResponse;
+import com.soma.group.dto.request.GroupJoinRequest;
+import com.soma.group.dto.response.GroupNameResponse;
+import com.soma.group.factory.fixtures.GroupFixtures;
 import com.soma.group.repository.GroupRepository;
-import com.soma.joinlist.repository.JoinListRepository;
+import com.soma.joinList.repository.JoinListRepository;
 import com.soma.member.factory.fixtures.MemberFixtures;
 import com.soma.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -16,8 +19,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static com.soma.group.factory.dto.GroupCreateFactory.createGroupCreateRequest;
+import static com.soma.group.factory.dto.GroupCreateFactory.createGroupJoinRequest;
+import static com.soma.group.factory.entity.GroupFactory.createGroup;
 import static com.soma.member.factory.entity.MemberFactory.createUserRoleMember;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -44,9 +50,42 @@ class GroupServiceTest {
         given(memberRepository.findByEmailAndStatus(anyString(), any())).willReturn(Optional.of(createUserRoleMember()));
 
         // when
-        GroupCreateResponse response = groupService.create(request, MemberFixtures.이메일);
+        GroupNameResponse response = groupService.create(request, MemberFixtures.이메일);
 
         // then
         assertThat(response.getGroupName()).isEqualTo(request.getGroupName());
+    }
+
+    @DisplayName("그룹에 가입한다.")
+    @Test
+    void join() throws Exception {
+        //given
+        GroupJoinRequest request = createGroupJoinRequest();
+
+        //mocking
+        given(groupRepository.findByCode(anyString())).willReturn(Optional.of(createGroup()));
+        given(memberRepository.findByEmailAndStatus(anyString(), any())).willReturn(Optional.of(createUserRoleMember()));
+        given(joinListRepository.existsByGroupAndMember(any(), any())).willReturn(false);
+
+        // when
+        GroupNameResponse response = groupService.join(request, MemberFixtures.이메일);
+
+        // then
+        assertThat(response.getGroupName()).isEqualTo(GroupFixtures.그룹명);
+    }
+
+    @DisplayName("기존에 가입된 그룹에 재가입은 불가능하다.")
+    @Test
+    void canNotJoinAlreadyJoinedGroup() throws Exception {
+        //given
+        GroupJoinRequest request = createGroupJoinRequest();
+
+        //mocking
+        given(groupRepository.findByCode(anyString())).willReturn(Optional.of(createGroup()));
+        given(memberRepository.findByEmailAndStatus(anyString(), any())).willReturn(Optional.of(createUserRoleMember()));
+        given(joinListRepository.existsByGroupAndMember(any(), any())).willReturn(true);
+
+        // when  // then
+        assertThatThrownBy(() -> groupService.join(request, MemberFixtures.이메일)).isInstanceOf(AlreadyJoinedGroupException.class);
     }
 }
