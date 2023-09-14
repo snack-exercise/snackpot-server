d2a249898c1ee31b96f31f75522c91683542288f
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-14 23:14:50 +0900

    [SNK-73] feat : 그룹 가입 GroupRepository 단위 테스트

---
+++ snackpot-api/src/test/java/com/soma/joinlist/repository/JoinListRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.soma.joinlist.repository;
+
+import com.soma.config.QueryDSLConfig;
+import com.soma.group.entity.Group;
+import com.soma.group.factory.entity.GroupFactory;
+import com.soma.group.repository.GroupRepository;
+import com.soma.joinList.repository.JoinListRepository;
+import com.soma.joinlist.entity.JoinList;
+import com.soma.joinlist.entity.JoinType;
+import com.soma.member.entity.Member;
+import com.soma.member.factory.entity.MemberFactory;
+import com.soma.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@Import(QueryDSLConfig.class) // @DataJpaTest는 JPA관련 빈만 등록, @JpaQueryFactory도 빈으로 등록해야함
+@DisplayName("JoinListRepository JPA 동작 테스트")
+class JoinListRepositoryTest {
+    @Autowired private JoinListRepository joinListRepository;
+    @Autowired private GroupRepository groupRepository;
+    @Autowired private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원이 그룹을 가입하지 않으면 false를 반환한다.")
+    void existsByGroupAndMemberFalse() throws Exception {
+        //given
+        Group group = GroupFactory.createGroup();
+        groupRepository.save(group);
+        Member member = MemberFactory.createUserRoleMember();
+        memberRepository.save(member);
+
+        //when
+        boolean result = joinListRepository.existsByGroupAndMember(group, member);
+
+        //then
+        Assertions.assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("회원이 그룹을 가입했다면 true를 반환한다.")
+    void existsByGroupAndMemberTrue() throws Exception {
+        //given
+        Group group = GroupFactory.createGroup();
+        groupRepository.save(group);
+        Member member = MemberFactory.createUserRoleMember();
+        memberRepository.save(member);
+        joinListRepository.save(
+                JoinList.builder()
+                .joinType(JoinType.HOST)
+                .group(group)
+                .member(member)
+                .build()
+        );
+
+        //when
+        boolean result = joinListRepository.existsByGroupAndMember(group, member);
+
+        //then
+        Assertions.assertThat(result).isTrue();
+    }
+}
