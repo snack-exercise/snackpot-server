984e8a28859e08d0db8c9aff9d33094365a95ccc
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-14 23:19:19 +0900

    [SNK-73] feat : 테스트용 Group 엔티티 factory 추가

---
+++ snackpot-api/src/test/java/com/soma/group/factory/entity/GroupFactory.java
@@ -0,0 +1,16 @@
+package com.soma.group.factory.entity;
+
+import com.soma.group.entity.Group;
+import com.soma.group.factory.fixtures.GroupFixtures;
+
+import java.time.LocalDate;
+
+public class GroupFactory {
+    public static Group createGroup(){
+        return Group.builder()
+                .name(GroupFixtures.그룹명)
+                .code(GroupFixtures.그룹코드)
+                .startDate(LocalDate.now())
+                .build();
+    }
+}
