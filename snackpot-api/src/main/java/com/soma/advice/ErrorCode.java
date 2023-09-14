91184927119beef0b20fa4105613feb900aebdd1
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-14 23:07:07 +0900

    [SNK-73] feat : 그룹 가입 관련 ErrorCode 추가

--- snackpot-api/src/main/java/com/soma/advice/ErrorCode.java
+++ snackpot-api/src/main/java/com/soma/advice/ErrorCode.java
@@ -8,7 +8,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
     /* Member */
-    MEMBER_NOT_FOUND_EXCEPTION(-1100, "사용자가 존재하지 않습니다.");
+    MEMBER_NOT_FOUND_EXCEPTION(-1100, "사용자가 존재하지 않습니다."),
+
+    /* Group */
+    ALREADY_JOINED_GROUP_EXCEPTION(-1200, "이미 가입한 그룹입니다."),
+    GROUP_NOT_FOUND_EXCEPTION(-1201, "그룹이 존재하지 않습니다.");
 
     private final int code;
     private final String message;
