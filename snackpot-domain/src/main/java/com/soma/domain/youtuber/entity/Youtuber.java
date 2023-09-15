013597ddeb111638780b5c46027c121c8c1ad7bb
ohjinseo <ojs835@naver.com>
2023-09-15 13:13:54 +0900

    [SNK-82] feat : Youtuber 엔티티 빌더 생성자 추가

--- snackpot-domain/src/main/java/com/soma/domain/youtuber/entity/Youtuber.java
+++ snackpot-domain/src/main/java/com/soma/domain/youtuber/entity/Youtuber.java
@@ -3,6 +3,7 @@ package com.soma.domain.youtuber.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,4 +21,12 @@ public class Youtuber {
     private String name;
 
     private String profileImg;
+
+    @Builder
+    public Youtuber(String channelId, String description, String name, String profileImg) {
+        this.channelId = channelId;
+        this.description = description;
+        this.name = name;
+        this.profileImg = profileImg;
+    }
 }
