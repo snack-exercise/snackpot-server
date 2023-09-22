84faf85002068cb7a3beea21c4875a04f10ee65b
ohjinseo <ojs835@naver.com>
2023-09-22 14:56:32 +0900

    [SNK-79] feat : 운동 상세정보 조회하기 API 추가

--- snackpot-api/src/main/java/com/soma/domain/exercise/service/ExerciseService.java
+++ snackpot-api/src/main/java/com/soma/domain/exercise/service/ExerciseService.java
@@ -1,19 +1,25 @@
 package com.soma.domain.exercise.service;
 
-import com.soma.domain.exercise.repository.ExerciseRepository;
-import com.soma.exception.exercise.ExerciseNotFoundException;
-import com.soma.exception.member.MemberNotFoundException;
+import com.soma.domain.bodypart.entity.BodyPart;
+import com.soma.domain.bodypart.repository.BodyPartRepository;
 import com.soma.domain.exercise.dto.request.ExerciseFinishRequest;
 import com.soma.domain.exercise.dto.request.ExerciseReadCondition;
+import com.soma.domain.exercise.dto.response.ExerciseDetailResponse;
 import com.soma.domain.exercise.dto.response.ExerciseResponse;
 import com.soma.domain.exercise.entity.Exercise;
+import com.soma.domain.exercise.repository.ExerciseRepository;
+import com.soma.domain.exercise_like.repository.ExerciseLikeRepository;
 import com.soma.domain.exercise_record.repository.ExerciseRecordRepository;
 import com.soma.domain.member.entity.Member;
 import com.soma.domain.member.repository.MemberRepository;
+import com.soma.exception.exercise.ExerciseNotFoundException;
+import com.soma.exception.member.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static com.soma.common.constant.Status.ACTIVE;
 
@@ -24,6 +30,9 @@ public class ExerciseService {
     private final ExerciseRepository exerciseRepository;
     private final ExerciseRecordRepository exerciseRecordRepository;
     private final MemberRepository memberRepository;
+    private final BodyPartRepository bodyPartRepository;
+
+    private final ExerciseLikeRepository exerciseLikeRepository;
 
     public Slice<ExerciseResponse> readAllByCondition(ExerciseReadCondition condition, int size, String email) {
         return exerciseRepository.findAllByCondition(condition, size, email);
@@ -34,5 +43,13 @@ public class ExerciseService {
         Member member = memberRepository.findByEmailAndStatus(email, ACTIVE).orElseThrow(MemberNotFoundException::new);
         Exercise exercise = exerciseRepository.findByIdAndStatus(request.getExerciseId(), ACTIVE).orElseThrow(ExerciseNotFoundException::new);
         exerciseRecordRepository.save(ExerciseFinishRequest.toEntity(exercise, member));
+    }
+
+    public ExerciseDetailResponse readExerciseDetails(Long exerciseId, String email) {
+        Exercise exercise = exerciseRepository.findByIdAndStatus(exerciseId, ACTIVE).orElseThrow(ExerciseNotFoundException::new);
+        List<BodyPart> bodyPartList = bodyPartRepository.findAllByExerciseIdAndStatus(exerciseId, ACTIVE);
+        Member member = memberRepository.findByEmailAndStatus(email, ACTIVE).orElseThrow(MemberNotFoundException::new);
+        Boolean isLike = exerciseLikeRepository.existsByMemberAndExercise(member, exercise);
+        return ExerciseDetailResponse.toDto(exercise, bodyPartList, isLike);
     }
 }
