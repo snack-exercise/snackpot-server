2257da39369dd5a7ebc1925d0395e90eaa6b33ab
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-25 22:33:31 +0900

    [SNK-97] feat : Swagger 세팅

--- snackpot-api/src/main/java/com/soma/SnackpotApiApplication.java
+++ snackpot-api/src/main/java/com/soma/SnackpotApiApplication.java
@@ -1,13 +1,21 @@
 package com.soma;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Slf4j
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "https://dev-api.snackexercise.com")})
 @SpringBootApplication
 @EnableJpaAuditing
 public class SnackpotApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SnackpotApiApplication.class, args);
     }
