a7567b9657420c05d49853ad8df6d8a27a81dac0
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-26 11:50:41 +0900

    [SNK-74] fix : Youtuber 엔티티 @GeneratedValue 추가

--- snackpot-domain/src/main/java/com/soma/domain/youtuber/entity/Youtuber.java
+++ snackpot-domain/src/main/java/com/soma/domain/youtuber/entity/Youtuber.java
@@ -1,6 +1,8 @@
 package com.soma.domain.youtuber.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Youtuber {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String channelId;
