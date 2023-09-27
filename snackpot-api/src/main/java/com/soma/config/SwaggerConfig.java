78a7189dc2dafa9297c52bce60acc0ef9d4ffa75
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-09-27 11:33:40 +0900

    [SNK-94] fix : Swagger Authorizataion Header 인식 불가능 해결

--- snackpot-api/src/main/java/com/soma/config/SwaggerConfig.java
+++ snackpot-api/src/main/java/com/soma/config/SwaggerConfig.java
@@ -8,18 +8,27 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
+
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                // Security 인증 컴포넌트 설정
+                .components(new Components().addSecuritySchemes("Authorization",
+                // API 마다 Security 인증 컴포넌트 설정
+                new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("Authorization")
+                        .in(SecurityScheme.In.HEADER)
+                        .name(HttpHeaders.AUTHORIZATION)))
                 .info(apiInfo());
     }
 
@@ -35,4 +44,6 @@ public class SwaggerConfig {
         schema.example(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))).type("string");
         SpringDocUtils.getConfig().replaceWithSchema(LocalTime.class, schema);
     }
+
+
 }
