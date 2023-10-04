6bed410c7c7fe775493a07178353dbfdc5318aad
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-10-04 13:29:12 +0900

    fix : Enumerated Ordinal에서 String 타입으로 수정

--- snackpot-domain/src/main/java/com/soma/domain/bodypart/entity/BodyPart.java
+++ snackpot-domain/src/main/java/com/soma/domain/bodypart/entity/BodyPart.java
@@ -1,10 +1,7 @@
 package com.soma.domain.bodypart.entity;
 
 import com.soma.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,5 +14,6 @@ public class BodyPart extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private BodyPartType bodyPartType;
 }
