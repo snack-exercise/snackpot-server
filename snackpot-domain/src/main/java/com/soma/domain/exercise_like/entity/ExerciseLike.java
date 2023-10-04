f07eff499cf47d8339dc8cc425d5a0f49a418190
ohjinseo <ojs835@naver.com>
2023-10-04 22:57:19 +0900

    [SNK-93] feat : ExerciseLike 빌더 생성자 추가

--- snackpot-domain/src/main/java/com/soma/domain/exercise_like/entity/ExerciseLike.java
+++ snackpot-domain/src/main/java/com/soma/domain/exercise_like/entity/ExerciseLike.java
@@ -1,26 +1,33 @@
 package com.soma.domain.exercise_like.entity;
 
-import com.soma.common.BaseEntity;
+import com.soma.common.BaseTimeEntity;
 import com.soma.domain.exercise.entity.Exercise;
 import com.soma.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class ExerciseLike extends BaseEntity {
+public class ExerciseLike extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "memberId")
+    @JoinColumn(name = "memberId")
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "exerciseId")
+    @JoinColumn(name = "exerciseId")
     private Exercise exercise;
+
+    @Builder
+    public ExerciseLike(Member member, Exercise exercise) {
+        this.member = member;
+        this.exercise = exercise;
+    }
 }
