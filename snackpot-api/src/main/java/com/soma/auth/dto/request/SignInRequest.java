ca2ddcda4e36df4b54080b5a0ef339dcec10cb30
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-10-26 21:41:37 +0900

    fix : SignInRequest, SignUpRequest에서 fcmToken 제거

--- snackpot-api/src/main/java/com/soma/auth/dto/request/SignInRequest.java
+++ snackpot-api/src/main/java/com/soma/auth/dto/request/SignInRequest.java
@@ -13,7 +13,4 @@ public class SignInRequest {
     @NotBlank(message = "이름은 필수입니다.")
     @Size(min = 1, max = 6, message = "이름은 1자 이상 6자 이하로 입력해주세요.")
     private String name;
-
-    @NotBlank(message = "fcm 토큰은 필수 값입니다.")
-    private String fcmToken;
 }
