68977d03c6c38d0fe4685723cdc7d1b3f5c45214
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-10-26 21:43:50 +0900

    feat : FCM_TOKEN_NOT_FOUND_EXCEPTION 추가

--- snackpot-api/src/main/java/com/soma/advice/ErrorCode.java
+++ snackpot-api/src/main/java/com/soma/advice/ErrorCode.java
@@ -16,8 +16,11 @@ public enum ErrorCode {
     GROUP_NOT_FOUND_EXCEPTION(-1201, "그룹이 존재하지 않습니다."),
 
     /* Exercise */
-    EXERCISE_NOT_FOUND_EXCEPTION(-1300, "운동이 존재하지 않습니다.");
+    EXERCISE_NOT_FOUND_EXCEPTION(-1300, "운동이 존재하지 않습니다."),
 
+
+    /* Notification */
+    FCM_TOKEN_NOT_FOUND_EXCEPTION(-1400, "FCM 토큰을 찾을 수 없습니다.");
     private final int code;
     private final String message;
 }
