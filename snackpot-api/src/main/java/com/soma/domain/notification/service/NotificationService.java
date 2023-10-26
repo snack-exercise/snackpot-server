026823566f31e0bdb6df63703db7aa7955f5dd4e
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-10-26 21:43:19 +0900

    fix : 콕 찌르기 API 호출시, 요청한 사람의 fcmToken 저장

--- snackpot-api/src/main/java/com/soma/domain/notification/service/NotificationService.java
+++ snackpot-api/src/main/java/com/soma/domain/notification/service/NotificationService.java
@@ -5,6 +5,7 @@ import com.soma.domain.group.entity.Group;
 import com.soma.domain.notification.NotificationMessage;
 import com.soma.domain.notification.dto.request.NotificationCreateRequest;
 import com.soma.exception.group.GroupNotFoundException;
+import com.soma.exception.member.FCMTokenNotFoundException;
 import com.soma.exception.member.MemberNotFoundException;
 import com.soma.domain.group.repository.GroupRepository;
 import com.soma.domain.member.entity.Member;
@@ -40,11 +41,15 @@ public class NotificationService {
     }
 
 
+    @Transactional(noRollbackFor = FCMTokenNotFoundException.class)
     public void create(NotificationCreateRequest request, String email) {
         Group group = groupRepository.findById(request.getGroupId()).orElseThrow(GroupNotFoundException::new);
         Member fromMember = memberRepository.findByEmailAndStatus(email, ACTIVE).orElseThrow(MemberNotFoundException::new);
+        fromMember.updateFcmToken(request.getFcmToken());
         Member toMember = memberRepository.findByIdAndStatus(request.getToUserId(), ACTIVE).orElseThrow(MemberNotFoundException::new);
-
+        if(toMember.getFcmToken() == null){
+            throw new FCMTokenNotFoundException();
+        }
         int idx = random.nextInt(notificationMessages.size());
 
         firebaseCloudMessageService.sendByToken(toMember.getFcmToken(), notificationMessages.get(idx).getTitleWithNicknameAndGroupName(group.getName(), fromMember.getName()), notificationMessages.get(idx).getBody());
